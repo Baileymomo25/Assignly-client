744e22b21f23e0801c377e3790d986aa24920054
Baileymomo25 <bailey.momo25@gmail.com>
2025-10-09 12:40:15 +0100

    please work

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Temporary Firebase stub
+export const initializeApp = () => {};
+export const getStorage = () => {};
+export const getFirestore = () => {};
+export const ref = () => {};
+export const uploadBytes = () => {};
+export const getDownloadURL = () => {};
+export const doc = () => {};
+export const setDoc = () => {};
+export const getDocs = () => {};
+export const collection = () => {};
+export const addDoc = () => {};
+export const getAuth = () => {};
+export const signInWithEmailAndPassword = () => {};
+export const createUserWithEmailAndPassword = () => {};
+export const onAuthStateChanged = () => {};
